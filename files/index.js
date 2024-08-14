a31028817d55fa0388d37b99945a4c7227f6cfb6
her-s <97346397+her-s@users.noreply.github.com>
2024-08-14 08:43:35 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
 const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
-const UUID = process.env.UUID || '00fda29c-8089-482b-87da-09935d5777dc'; //若需要改UUID，需要在config.json里改为一致
+const UUID = process.env.UUID || '2455ce97-7884-44f7-8ba8-6570cbc89255'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo.dyhdg.tk
-';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWU2ZTI0MWExM2RkNjIzZDQ5YzIyMTJiNmQ0YjZjYWMiLCJ0IjoiMDgyODgwMjctNThkZS00YzNlLWFkZjctOTM0Y2I5NTgwNzQxIiwicyI6Ill6ZG1OVEV3TXpndFltWXhNaTAwTkRGbUxUZzVNekl0TW1ZMU9HWTJNRGN6WkROaiJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || '';
 const CFIP = process.env.CFIP || 'visa.com.sg';
-const NAME = process.env.NAME || 'Choreo-202407';
+const NAME = process.env.NAME || 'Choreo-202408';
 
 // root route
 app.get("/", function(req, res) {
